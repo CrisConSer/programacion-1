2d4e12fa337df70844383b3fe76f092894f58a8c
cmilsor245 <cmilsor245@g.educaand.es>
2023-01-14 19:08:14 +0100

    cms

--- tema7/c71/ej14/Ej14.java
+++ tema7/c71/ej14/Ej14.java
@@ -5,8 +5,34 @@
 
 package tema7.c71.ej14;
 
+import java.util.Scanner;
+
 public class Ej14{
   public static void main(String[] args){
-    
+    // array
+      // número de elementos
+        int t=8;
+
+      // creación
+        String[] n=new String[t];
+
+    /*****************************/
+
+    // elementos
+      // scanner
+        Scanner s=new Scanner(System.in);
+
+      // palabras
+        for(int i=0; i<t; i++){
+          System.out.print("Introduce una palabra: ");
+            n[i]=s.nextLine();
+        }
+
+      // cierre scanner
+        s.close();
+
+    /*****************************/
+
+    // 
   }
 }
