94a1106167e8881b98fcbeec81ec482e23d6c493
cmilsor245 <cmilsor245@g.educaand.es>
2023-01-20 17:43:16 +0100

    cms

---
+++ tema7/c72/ej18_bidi/Ej18.java
@@ -0,0 +1,12 @@
+/*
+  @author: Christian Millán Soria
+  info: ejercicio 18 bidimensionales
+*/
+
+package tema7.c72.ej18_bidi;
+
+public class Ej18{
+  public static void main(String[] args){
+    
+  }
+}
