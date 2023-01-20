ed52370b94e1dd83ec7a872d759f2936e3876a37
cmilsor245 <cmilsor245@g.educaand.es>
2023-01-20 01:13:45 +0100

    cms

---
+++ tema7/c72/ej4_bidi/Ej4.java
@@ -0,0 +1,12 @@
+/*
+  @author: Christian Millán Soria
+  info: ejercicio 4 bidimensionales
+*/
+
+package tema7.c72.ej4_bidi;
+
+public class Ej4{
+  public static void main(String[] args){
+    
+  }
+}
---
+++ tema7/c72/ej5_bidi/Ej5.java
@@ -0,0 +1,12 @@
+/*
+  @author: Christian Millán Soria
+  info: ejercicio 5 bidimensionales
+*/
+
+package tema7.c72.ej5_bidi;
+
+public class Ej5{
+  public static void main(String[] args){
+    
+  }
+}
