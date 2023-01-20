a273d008b5ed3d9b880eab7a3609dc89d3cfc43a
cmilsor245 <cmilsor245@g.educaand.es>
2023-01-20 18:16:46 +0100

    cms

--- tema7/c72/ej18_bidi/Ej18.java
+++ tema7/c72/ej18_bidi/Ej18.java
@@ -7,6 +7,42 @@ package tema7.c72.ej18_bidi;
 
 public class Ej18{
   public static void main(String[] args){
-    
+    // creación del array
+      // número de elementos
+        int t=10;
+
+      // creación
+        int[] array=new int[t];
+
+    /*************************************/
+
+    // asignación de valores aleatorios entre 0-100
+      for(int i=0; i<t; i++){
+        array[i]=(int)(Math.random()*201);
+      }
+
+    /*************************************/
+
+    // índice
+      int indice=0;
+
+    /*************************************/
+
+    // reordenamiento
+      
+
+    /*************************************/
+
+    // debug
+      System.out.println("+--------+----+----+----+----+----+----+----+----+----+----+");
+      System.out.print("| Indice ");
+
+      for(int i=0; i<10; i++){
+        System.out.printf("| %-2d ", indice);
+
+        indice++;
+      }
+
+      System.out.println("|");
   }
 }
