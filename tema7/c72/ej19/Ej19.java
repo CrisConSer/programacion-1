398707ffc41536924a3e0495b06b3ede5f55236d
cmilsor245 <cmilsor245@g.educaand.es>
2023-01-22 00:16:56 +0100

    cms

--- tema7/c72/ej19/Ej19.java
+++ tema7/c72/ej19/Ej19.java
@@ -7,6 +7,33 @@ package tema7.c72.ej19;
 
 public class Ej19{
   public static void main(String[] args){
-    
+    // array original
+      // número de elementos
+        int t=12;
+
+      // creación
+        int[] array=new int[t];
+
+      // asignación de valores aleatorios
+        for(int i=0; i<t; i++){
+          array[i]=(int)(Math.random()*201);
+        }
+
+      /*****************************************/
+
+      // muestra array original
+        int indice=0;
+
+        System.out.print("Indice ");
+        for(int i=0; i<12; i++){
+          System.out.printf(" %-3d ", indice);
+  
+          indice++;
+        }
+
+        System.out.print("\nValor  ");
+        for(int i=0; i<12; i++){
+          System.out.printf(" %-3d ", array[i]);
+        }
   }
 }
