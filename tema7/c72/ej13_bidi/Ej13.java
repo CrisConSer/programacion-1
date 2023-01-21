4e29fa6d1bedc381746a394d3b2a38c84c7519e6
cmilsor245 <cmilsor245@g.educaand.es>
2023-01-21 23:47:40 +0100

    cms

--- tema7/c72/ej13_bidi/Ej13.java
+++ tema7/c72/ej13_bidi/Ej13.java
@@ -7,6 +7,42 @@ package tema7.c72.ej13_bidi;
 
 public class Ej13{
   public static void main(String[] args){
-    
+    // array países
+      // creación
+        String[] pais={"España", "Rusia", "Japón", "Australia"};
+
+    /**********************************************/
+
+    // array estaturas
+      // dimensiones
+        int f=4;
+        int c=10;
+
+      // creación
+        int[][] estatura=new int[f][c];
+
+      // asignación de valores
+        for(int i=0; i<f; i++){
+          for(int j=0; j<c; j++){
+            estatura[i][j]=(int)(Math.random()*71+140);
+          }
+        }
+
+    /**********************************************/
+
+    // debug
+      String indice="MED MIN MAX ";
+
+      System.out.println("                                                "+indice);
+
+      for(int i=0; i<f; i++){
+        System.out.printf("%9s:", pais[i]);
+
+        for(int k=0; k<c; k++){
+          System.out.printf(" %-3d ", estatura[i][k]);
+        }
+
+        System.out.println();
+      }
   }
 }
