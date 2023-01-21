b4904f2cac1f5cdd2ccb72f68a4509897320825f
cmilsor245 <cmilsor245@g.educaand.es>
2023-01-21 23:28:14 +0100

    cms

---
+++ tema7/c72/ej13_bidi/Ej13.java
@@ -0,0 +1,12 @@
+/*
+  @author: Christian Millán Soria
+  info: ejercicio 13 bidimensionales
+*/
+
+package tema7.c72.ej13_bidi;
+
+public class Ej13{
+  public static void main(String[] args){
+    
+  }
+}
