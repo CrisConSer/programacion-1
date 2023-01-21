884c05083fe99646f5f9c984b56d52b03bf818d5
cmilsor245 <cmilsor245@g.educaand.es>
2023-01-21 22:26:03 +0100

    cms

--- tema7/c72/ej11_bidi/Ej11.java
+++ tema7/c72/ej11_bidi/Ej11.java
@@ -22,11 +22,60 @@ public class Ej11{
           }
         }
 
-      // debug
-        for(int i=0; i<filas; i++){
-          for(int j=0; j<columnas; j++){
-            System.out.println(array[i][j]);
+    /**********************************/
+
+    // array original
+      for(int i=0; i<filas; i++){
+        for(int j=0; j<columnas; j++){
+          System.out.printf("| %-3d ", array[i][j]);
+        }
+
+        System.out.println("|");
+      }
+
+      System.out.println();
+      System.out.println();
+
+    /**********************************/
+
+    // valores en la diagonal
+      // máximo, mínimo y media
+        int suma=0;
+
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+
+        int totalNums=0;
+
+      /*******************/
+
+      for(int i=0; i<filas; i++){
+        for(int j=0; j<columnas; j++){
+          if(i==j){// debido a que las casillas que forma la diagonal siempre coinciden en el valor de fila y columna...
+            System.out.printf("| %-3d ", array[i][j]);
+
+            totalNums++;
+
+            suma+=array[i][j];
+
+            // máximo
+              if(array[i][j]>max){
+                max=array[i][j];
+              }
+
+            // mínimo
+              if(array[i][j]<min){
+                min=array[i][j];
+              }
           }
         }
+      }
+
+      System.out.println("|");
+
+      /*******************/
+
+      // debug máximo, mínimo y media
+        System.out.print("\nEl valor máximo de entre los que conforman la diagonal es el "+max+", el mínimo es el "+min+"y la media de todos estos valores da como resultado "+suma/totalNums+".");
   }
 }
