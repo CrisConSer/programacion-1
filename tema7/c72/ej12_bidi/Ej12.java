97dd2a9b247a8a4e7cd6db96f769b26b238b9683
cmilsor245 <cmilsor245@g.educaand.es>
2023-01-21 22:26:41 +0100

    cms

---
+++ tema7/c72/ej12_bidi/Ej12.java
@@ -0,0 +1,12 @@
+/*
+  @author: Christian Millán Soria
+  info: ejercicio 12 bidimensionales
+*/
+
+package tema7.c72.ej12_bidi;
+
+public class Ej12{
+  public static void main(String[] args){
+    
+  }
+}
