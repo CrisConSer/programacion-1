b1478222d687e5d9c496b275a75e0d52e6f25fff
cmilsor245 <cmilsor245@g.educaand.es>
2023-01-22 00:06:45 +0100

    cms

---
+++ tema7/c72/ej19/Ej19.java
@@ -0,0 +1,12 @@
+/*
+  @author: Christian Millán Soria
+  info: ejercicio 19
+*/
+
+package tema7.c72.ej19;
+
+public class Ej19{
+  public static void main(String[] args){
+    
+  }
+}
---
+++ tema7/c72/ej21/Ej21.java
@@ -0,0 +1,12 @@
+/*
+  @author: Christian Millán Soria
+  info: ejercicio 21
+*/
+
+package tema7.c72.ej21;
+
+public class Ej21{
+  public static void main(String[] args){
+    
+  }
+}
