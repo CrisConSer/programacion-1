f87f989fe376e7d27d2409752098466062d686dd
cmilsor245 <cmilsor245@g.educaand.es>
2023-01-27 17:46:25 +0100

    cms

---
+++ tema8/c81/ej1/Ej1.java
@@ -0,0 +1,13 @@
+/**
+  @author: Christian Millán Soria
+  @file: Ej1.java
+  @info: ejercicio 1
+*/
+
+package tema8.c81.ej1;
+
+public class Ej1{
+  public static void main(String[] args){
+    
+  }
+}
