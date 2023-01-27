e094024636b239a855d542c9c83e76cd624a87a8
cmilsor245 <cmilsor245@g.educaand.es>
2023-01-27 18:50:31 +0100

    cms

--- tema8/c81/ej1/Ej1.java
+++ tema8/c81/ej1/Ej1.java
@@ -6,8 +6,56 @@
 
 package tema8.c81.ej1;
 
+import java.util.Scanner;
+
 public class Ej1{
-  public static void main(String[] args){
-    
-  }
+  /**
+    @info: función para números capicúa
+    @param x : número
+    @return:
+      - true: el número es capicúa
+      - false: el número no es capicúa
+  */
+
+    public static boolean esCapicua(int x){
+      // variable auxiliar
+        int aux=x;
+
+      // número final
+        int numAlreves=0;
+
+      // volteo
+        while(aux>0){
+          int ultimoDigi=aux%10;
+
+          numAlreves=numAlreves*10+ultimoDigi;
+
+          aux=aux/10;
+        }
+    }
+
+  // main
+    public static void main(String[] args){
+      // número
+        // scanner
+          Scanner s=new Scanner(System.in);
+
+        // obtención del número
+          System.out.print("Introduce el número: ");
+            int n=s.nextInt();
+
+          System.out.println();
+
+        // cierre scanner
+          s.close();
+
+      /************************************/
+
+      // función "esCapicua"
+        if(esCapicua(n)){
+          System.out.print("\nEl número "+n+" es capicúa.");
+        }else{
+          System.out.print("\nEl número "+n+" no es capicúa.");
+        }
+    }
 }
