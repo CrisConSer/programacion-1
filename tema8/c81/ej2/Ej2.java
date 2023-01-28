a6d9ed77401ed3938869ff02efc3a007a26722b1
Christian Millán Soria <cmilsor245@g.educaand.es>
2023-01-28 22:37:49 +0100

    cms

--- tema8/c81/ej2/Ej2.java
+++ tema8/c81/ej2/Ej2.java
@@ -1,11 +1,65 @@
 /**
   @author: Christian Millán Soria
-  @file: Ej2.java
-  @info: ejercicio 2
+  @file: ej2.java
+  @info: ejercicio 1
 */
 
 package tema8.c81.ej2;
 
+import java.util.Scanner;
+
 public class Ej2{
-  ejemplo
+  /**
+    @info: función para números capicúa
+    @param x: número
+    @return:
+      - true: el número es capicúa
+      - false: el número no es capicúa
+  */
+
+    public static boolean esCapicua(int x){
+      // variable auxiliar
+        int aux=x;
+
+      // número invertido
+        int rev=0;
+
+      // volteo
+        while(aux!=0) {
+            rev=rev*10+aux%10;
+            aux/=10;
+        }        
+
+      // comprobación
+        if(x==rev){
+          return true;
+        }else{
+          return false;
+        }
+    }
+
+  /***************************************************************/
+
+  // main
+    public static void main(String[] args){
+      // número
+        // scanner
+          Scanner s=new Scanner(System.in);
+
+        // obtención del número
+          System.out.print("Introduce el número: ");
+            int n=s.nextInt();
+
+        // cierre scanner
+          s.close();
+
+      /************************************/
+
+      // función "esCapicua"
+        if(esCapicua(n)){
+          System.out.print("\nEl número "+n+" es capicúa.");
+        }else{
+          System.out.print("\nEl número "+n+" no es capicúa.");
+        }
+    }
 }
