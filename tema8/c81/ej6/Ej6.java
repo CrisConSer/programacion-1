b048ce78f6f7b16e022cd7816acd10ec4e53c1ac
Christian Millán Soria <cmilsor245@g.educaand.es>
2023-01-29 18:40:24 +0100

    cms

---
+++ tema8/c81/ej6/Ej6.java
@@ -0,0 +1,59 @@
+/**
+  @author: Christian Millán Soria
+  @file: Ej6.java
+  @info: ejercicio 6
+*/
+
+package tema8.c81.ej6;
+
+import java.util.Scanner;
+
+public class Ej6{
+  /**
+    @info: función para números primo
+    @param x: número
+    @return:
+      - "numDigi": número de dígitos que tiene el número introducido
+  */
+    public static int digitos(int x){
+      int numDigi=0;
+
+      // conteo
+        // bucle
+          for(int i=0; i<x; i++){
+            while(x%10!=0){
+              x/=10;
+
+              numDigi++;
+            }
+          }
+
+        /*
+          alternativa: convertirlo a string y sacar su longitud:
+            numDigi=String.valueOf(x).length();
+        */
+
+      return numDigi;
+    }
+
+  /***************************************************************/
+
+  // main
+    public static void main(String[] args){
+      // números
+        // scanner
+          Scanner s=new Scanner(System.in);
+
+        // obtención del número
+          System.out.print("Introduce el número: ");
+            int n=s.nextInt();
+
+        // cierre scanner
+          s.close();
+
+      /************************************/
+
+      // función "digitos"
+        System.out.println("\nEl número "+n+" tiene "+digitos(n)+" dígitos.");
+    }
+}
