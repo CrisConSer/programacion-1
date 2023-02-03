cefd91ea04c0a567086ced62faee247757eb57c1
Christian Millán Soria <cmilsor245@g.educaand.es>
2023-02-03 15:59:50 +0100

    cms

--- tema8/c81/Ej9Func.java
+++ tema8/c81/Ej9Func.java
@@ -6,6 +6,37 @@
 
 package tema8.c81;
 
+import tema8.c81.misFunciones.Varias;
+
+import java.util.Scanner;
+
 public class Ej9Func{
-  
+  public static void main(String[] args){
+    // números
+      // scanner
+        Scanner s=new Scanner(System.in);
+
+      /**************/
+
+        // primer número
+          System.out.print("Introduce el primer número: ");
+            int num1=s.nextInt();
+
+        /*********/
+
+        // segundo número
+          System.out.print("Introduce el segundo número: ");
+            int num2=s.nextInt();
+
+        /*********/
+
+        // tercer número
+          System.out.print("Introduce el tercer número: ");
+            int num3=s.nextInt();
+
+      /**************/
+
+      // cierre de scanner
+        s.close();
+  }
 }
