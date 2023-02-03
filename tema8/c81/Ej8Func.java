7bf885a024d77cb2cad6e8aeb8b16dba87a7ee2a
Christian Millán Soria <cmilsor245@g.educaand.es>
2023-02-03 15:47:36 +0100

    cms

---
+++ tema8/c81/Ej8Func.java
@@ -0,0 +1,17 @@
+/**
+  @author: Christian Millán Soria
+  @file: Ej8Func.java
+  @info: ejercicio 8 del listado de funciones
+*/
+
+package tema8.c81;
+
+import tema8.c81.misFunciones.Varias;
+
+import java.util.Scanner;
+
+public class Ej8Func{
+  public static void main(String[] args){
+    
+  }
+}
