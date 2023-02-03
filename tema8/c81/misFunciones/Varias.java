e7870f80f5cd04495691f8f129ea19f914b22d62
Christian Millán Soria <cmilsor245@g.educaand.es>
2023-02-03 01:26:14 +0100

    cms

--- tema8/c81/misFunciones/Varias.java
+++ tema8/c81/misFunciones/Varias.java
@@ -1,2 +1,31 @@
 package tema8.c81.misFunciones;
 
+public class Varias{
+  /**
+    @info: función para números capicúa
+    @param x: número
+    @return:
+      - "true": el número es capicúa
+      - "false": el número no es capicúa
+  */
+    public static boolean esCapicua(int x){
+      // variable auxiliar
+        int aux=x;
+
+      // número invertido
+        int rev=0;
+
+      // volteo
+        while(aux!=0) {
+            rev=rev*10+aux%10;
+            aux/=10;
+        }        
+
+      // comprobación
+        if(x==rev){
+          return true;
+        }else{
+          return false;
+        }
+    }
+}
