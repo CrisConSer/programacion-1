ebc24581bee5cd0e03f3a1fa3896579a45a122f8
Christian Millán Soria <cmilsor245@g.educaand.es>
2023-02-03 15:43:42 +0100

    cms

--- tema8/c81/Ej7Func.java
+++ tema8/c81/Ej7Func.java
@@ -6,6 +6,38 @@
 
 package tema8.c81;
 
+import tema8.c81.misFunciones.Varias;
+
+import java.util.Scanner;
+
 public class Ej7Func{
-  
+  public static void main(String[] args){
+    // área y perímetro
+      // scanner
+        Scanner s=new Scanner(System.in);
+
+        /**************/
+
+        // ancho
+          System.out.print("Introduce el ancho del rectángulo: ");
+            double ancho=s.nextDouble();
+
+        // alto
+          System.out.print("Introduce el alto del rectángulo: ");
+            double alto=s.nextDouble();
+
+        /**************/
+
+      // cierre de scanner
+        s.close();
+
+    /************************************/
+
+    // debug
+      // perímetro
+        System.out.println("\nEl perímetro de un rectángulo de ancho "+ancho+" y alto "+alto+" es: "+Varias.perimetroRectangulo(ancho,alto));
+
+      // área
+        System.out.print("\nEl área de un rectángulo de ancho "+ancho+" y alto "+alto+" es: "+Varias.areaRectangulo(ancho,alto));
+  }
 }
--- tema8/c81/misFunciones/Varias.java
+++ tema8/c81/misFunciones/Varias.java
@@ -270,4 +270,42 @@ public class Varias{
       // return
         return rev;
     }
+
+  /***************************************************/
+
+  /**
+    @info: función para calcular el perímetro de un rectángulo
+    @param:
+      - "ancho": ancho del rectángulo
+      - "alto": alto del rectángulo
+    @return perimetro: perímetro del rectángulo
+  */
+    public static double perimetroRectangulo(double ancho, double alto){
+      // variable para el perímetro
+        double perimetro=(ancho*=2)+(alto*=2);
+
+      /***************/
+
+      // return
+        return perimetro;
+    }
+
+  /***************************************************/
+
+  /**
+    @info: función para calcular el área de un rectángulo
+    @param:
+      - "ancho": ancho del rectángulo
+      - "alto": alto del rectángulo
+    @return area: área del rectángulo
+  */
+    public static double areaRectangulo(double ancho, double alto){
+      // variable para el perímetro
+        double area=ancho*alto;
+
+      /***************/
+
+      // return
+        return area;
+    }
 }
