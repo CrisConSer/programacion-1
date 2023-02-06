3611a727b59fedc079ebd7ad53b88fe6e7598d4f
Christian Millán Soria <cmilsor245@g.educaand.es>
2023-02-07 00:06:36 +0100

    cms

--- tema8/c81/Ej17Func.java
+++ tema8/c81/Ej17Func.java
@@ -6,6 +6,46 @@
 
 package tema8.c81;
 
+import tema8.c81.misFunciones.Varias;
+
+import java.util.Scanner;
+
 public class Ej17Func{
-  
+  public static void main(String[] args){
+    // variable para el número a leer
+      int num;
+
+    /**********************************************/
+
+    // números mientras no sea un 0
+      do{
+        // scanner
+          Scanner s=new Scanner(System.in);
+
+          /*****************/
+
+          // obtención
+            System.out.print("Introduce un número: ");
+              num=s.nextInt();
+
+          /*****************/
+
+        // cierre scanner
+          s.close();
+
+        /******************************/
+
+        // comprobación de si es primo
+          if(Varias.esPrimo(num)){
+            System.out.println("El número "+num+" es primo");
+          }else{
+            System.out.println("El número "+num+" no es primo");
+          }
+      }while(num!=0);
+
+    /**********************************************/
+
+    // el bucle termina
+      System.out.print("\nEl número introducido es un 0, por lo que termina el programa.");
+  }
 }
