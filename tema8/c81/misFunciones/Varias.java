2866968b0ba54a3dbd6c7bc4a35a9f582561dc39
Christian Millán Soria <cmilsor245@g.educaand.es>
2023-02-06 16:04:06 +0100

    cms

--- tema8/c81/misFunciones/Varias.java
+++ tema8/c81/misFunciones/Varias.java
@@ -399,8 +399,12 @@ public class Varias{
   */
     public static void printTiangulo(char c, int n){
       for(int i=1; i<=n; i++){
-        for(int j=1; j<=i; j++){
-            System.out.print(c+" ");
+        for(int j=1; j<=n-i; j++){
+          System.out.print(" ");
+        }
+
+        for(int j=1; j<=2*i-1; j++){
+          System.out.print(c);
         }
 
         System.out.println();
