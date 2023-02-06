a53cd2b645d79d30fb9f76fab2b3a634debab481
Christian Millán Soria <cmilsor245@g.educaand.es>
2023-02-06 16:11:42 +0100

    cms

---
+++ tema8/c81/Ej15Func.java
@@ -0,0 +1,13 @@
+/**
+  @author: Christian Millán Soria
+  @file: Ej15Func.java
+  @info: ejercicio 15 del listado de funciones
+*/
+
+package tema8.c81;
+
+public class Ej15Func{
+  public static void main(String[] args){
+    
+  }
+}
