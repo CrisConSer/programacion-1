6c2500d1fa85de92557f0c3096087b105dda031b
Christian Millán Soria <cmilsor245@g.educaand.es>
2023-02-09 15:43:35 +0100

    cms

--- tema8/vc82/Ej18.java
+++ tema8/vc82/Ej18.java
@@ -6,6 +6,21 @@
 
 package tema8.vc82;
 
+import tema8.vc82.misFunciones.Varias;
+
+import java.util.Scanner;
+
 public class Ej18{
-  
+  public static void main(String[] args){
+    // número a convertir
+      System.out.print("Introduce el número decimal ");
+
+      // scanner
+        Scanner s=new Scanner(System.in);
+          int num=s.nextInt();
+
+    /***************************************/
+
+    // 
+  }
 }
