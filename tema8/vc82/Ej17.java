34b8f5da4bf638d7e1c25f5b9ae272930d42302f
Christian Millán Soria <cmilsor245@g.educaand.es>
2023-02-09 13:40:36 +0100

    cms

--- tema8/vc82/Ej17.java
+++ tema8/vc82/Ej17.java
@@ -6,6 +6,26 @@
 
 package tema8.vc82;
 
+import tema8.vc82.misFunciones.Varias;
+
+import java.util.Scanner;
+
 public class Ej17{
-  
+  public static void main(String[] args){
+    // número a convertir
+      System.out.print("Introduce un número binario (sin decimales) para obtenerlo en decimal: ");
+
+      // scanner
+        Scanner s=new Scanner(System.in);
+          int num=s.nextInt();
+
+    /***********************************************/
+
+    // comprobación de si es binario
+      if(!Varias.esBinario(num)){
+        System.out.println("El número "+num+" no es binario.");
+      }else{
+        
+      }
+  }
 }
