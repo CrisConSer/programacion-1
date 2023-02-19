03853ea1f437fb922db8b32a758e4df22decdaf2
Christian Millán Soria <cmilsor245@g.educaand.es>
2023-02-19 19:07:00 +0100

    cms

--- tema6/c61/Ej1.java
+++ tema6/c61/Ej1.java
@@ -7,5 +7,32 @@
 package tema6.c61;
 
 public class Ej1{
-  
+  public static void main(String[] args){
+    // variable para la suma
+      int suma=0;
+
+    /*************************/
+
+    // variable auxiliar
+      int aux;
+
+    /*************************/
+
+    // tiradas y suma
+      System.out.println("Estas son las 3 tiradas de dados:");
+      System.out.println();
+
+      for(int i=0; i<3; i++){
+        aux=(int)(Math.random()*6+1);
+
+        System.out.println(aux);
+
+        suma+=aux;
+      }
+
+    /*************************/
+
+    // debug
+      System.out.print("\nLos puntos que suman las 3 tiradas son "+suma+".");
+  }
 }
---
+++ tema6/c61/Ej2.java
@@ -0,0 +1,13 @@
+/**
+  @author: Christian Millán Soria
+  @file: Ej2.java
+  @info: ejercicio 2
+*/
+
+package tema6.c61;
+
+public class Ej2{
+  public static void main(String[] args){
+    
+  }
+}
