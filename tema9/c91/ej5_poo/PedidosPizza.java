71e54afdf76b7f3bbef278b5ae926582ba985d8d
Christian Millán Soria <cmilsor245@g.educaand.es>
2023-02-24 17:55:13 +0100

    cms

--- tema9/c91/ej5_poo/PedidosPizza.java
+++ tema9/c91/ej5_poo/PedidosPizza.java
@@ -8,15 +8,16 @@ package tema9.c91.ej5_poo;
 
 public class PedidosPizza {
   public static void main(String[] args) {
-    Pizza p1 = new Pizza("margarita", "mediana");
-    Pizza p2 = new Pizza("funghi", "familiar");
-    p2.sirve();
-    Pizza p3 = new Pizza("cuatro quesos", "mediana");
-    System.out.println(p1);
-    System.out.println(p2);
-    System.out.println(p3);
-    p2.sirve();
-    System.out.println("pedidas: " + Pizza.getTotalPedidas());
-    System.out.println("servidas: " + Pizza.getTotalServidas());
+    // interacciones
+      Pizza pizza1=new Pizza("margarita", "mediana");
+      Pizza pizza2=new Pizza("4 quesos", "familiar");
+      pizza2.sirve();
+      Pizza p3=new Pizza("cuatro quesos", "mediana");
+      System.out.println(pizza1);
+      System.out.println(pizza2);
+      System.out.println(p3);
+      pizza2.sirve();
+      System.out.println("pedidas: "+Pizza.getTotalPedidas());
+      System.out.println("servidas: "+Pizza.getTotalServidas());
   }
 }
