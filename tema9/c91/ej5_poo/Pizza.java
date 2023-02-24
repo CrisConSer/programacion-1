e89f387b26b33918cbd3f4696177c65d88f9acc6
Christian Millán Soria <cmilsor245@g.educaand.es>
2023-02-24 19:33:43 +0100

    cms

--- tema9/c91/ej5_poo/Pizza.java
+++ tema9/c91/ej5_poo/Pizza.java
@@ -12,35 +12,54 @@ public class Pizza {
     private String tamano;
     private String estado;
 
-  // métodos
-    public Pizza(String tipo, String tamano){
-      this.tipo=tipo;
-      this.tamano=tamano;
-      this.estado="pedida";
-    }
-
-    public void sirve(){
-      if(estado.equals("pedida")){
-        estado="servida";
-        totalServidas++;
-      }else{
-        System.out.println("esa pizza ya se ha servido");
-      }
-    }
-
-    public String toString(){
-      return "pizza "+tipo+" "+tamano+", "+estado;
-    }
-
-    public static int getTotalPedidas(){
-      return totalPedidas;
-    }
-
-    public static int getTotalServidas(){
-      return totalServidas;
-    }
+  /************************************/
 
   // variables de clase
     private static int totalPedidas=0;
     private static int totalServidas=0;
+
+  /************************************/
+
+  // constructor
+    public Pizza(String tipo, String tamano){
+      // inicialización de los atributos
+        this.tipo=tipo;
+        this.tamano=tamano;
+        this.estado="pedida";
+    }
+
+  /************************************/
+
+  // métodos
+    // servir
+      public void sirve(){
+        if(estado.equals("pedida")){
+          estado="servida";
+
+          totalServidas++;
+        }else{
+          System.out.println("esa pizza ya se ha servido");
+        }
+      }
+
+    /*****************/
+
+    // método "toString" - representa una pizza en una cadena
+      public String toString(){
+        return "pizza "+tipo+" "+tamano+", "+estado;
+      }
+
+    /*****************/
+
+    // método "getTotalPedidas" - obtiene el número total de pizzas pedidas
+      public static int getTotalPedidas(){
+        return totalPedidas;
+      }
+
+    /*****************/
+
+    // método "getTotalServidas" - obtiene el número total de pizzas servidas
+      public static int getTotalServidas(){
+        return totalServidas;
+      }
 }
