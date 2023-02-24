435911d91aacf3c0a6184b7d82414ddf7de44038
Christian Millán Soria <cmilsor245@g.educaand.es>
2023-02-24 11:41:07 +0100

    cms

--- tema9/c91/ej3_poo/TestEj3.java
+++ tema9/c91/ej3_poo/TestEj3.java
@@ -8,14 +8,18 @@ package tema9.c91.ej3_poo;
 
 public class TestEj3{
   public static void main(String[] args){
-    // instancia de la clase "Ave"
+    // instancia de las clases creadas
+      Mamifero mamifero1=new Mamifero("Firulais", 2);
       Ave ave1=new Ave("Piolín", 5);
+      Gato gato1=new Gato("Garfield", 7);
+      Perro perro1=new Perro("Toby", 4);
+      Pinguino pinguino1=new Pinguino("Tux", 3);
+      Lagarto lagarto1=new Lagarto("Godzilla", 50);
 
     /*********************/
 
     // interacciones
-      ave1.volar();
-      System.out.println("Nombre de la ave: "+ave1.getNombre());
-      System.out.println("Edad de la ave: "+ave1.getEdad());
+      // ave
+        
   }
 }
