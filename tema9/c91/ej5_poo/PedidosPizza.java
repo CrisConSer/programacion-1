52fe71fd05077177a4d8c81a0b655de15c709dea
Christian Millán Soria <cmilsor245@g.educaand.es>
2023-02-24 17:51:15 +0100

    cms

---
+++ tema9/c91/ej5_poo/PedidosPizza.java
@@ -0,0 +1,22 @@
+/**
+  @author: Christian Millán Soria
+  @file: PedidosPizza.java
+  @info: clase "PedidosPizza" - ejercicio 5
+*/
+
+package tema9.c91.ej5_poo;
+
+public class PedidosPizza {
+  public static void main(String[] args) {
+    Pizza p1 = new Pizza("margarita", "mediana");
+    Pizza p2 = new Pizza("funghi", "familiar");
+    p2.sirve();
+    Pizza p3 = new Pizza("cuatro quesos", "mediana");
+    System.out.println(p1);
+    System.out.println(p2);
+    System.out.println(p3);
+    p2.sirve();
+    System.out.println("pedidas: " + Pizza.getTotalPedidas());
+    System.out.println("servidas: " + Pizza.getTotalServidas());
+  }
+}
--- tema9/c91/ej5_poo/Pizza.java
+++ tema9/c91/ej5_poo/Pizza.java
@@ -6,6 +6,41 @@
 
 package tema9.c91.ej5_poo;
 
-public class Pizza{
-  
+public class Pizza {
+  // atributos
+    private String tipo;
+    private String tamano;
+    private String estado;
+
+  // métodos
+    public Pizza(String tipo, String tamano){
+      this.tipo=tipo;
+      this.tamano=tamano;
+      this.estado="pedida";
+    }
+
+    public void sirve(){
+      if(estado.equals("pedida")){
+        estado="servida";
+        totalServidas++;
+      }else{
+        System.out.println("esa pizza ya se ha servido");
+      }
+    }
+
+    public String toString(){
+      return "pizza "+tipo+" "+tamano+", "+estado;
+    }
+
+    public static int getTotalPedidas(){
+      return totalPedidas;
+    }
+
+    public static int getTotalServidas(){
+      return totalServidas;
+    }
+
+  // variables de clase
+    private static int totalPedidas=0;
+    private static int totalServidas=0;
 }
