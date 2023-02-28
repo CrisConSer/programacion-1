4c4749fa1e9a612f0de99b6a64ba211a87dc5e7c
cmilsor245 <cmilsor245@g.educaand.es>
2023-02-28 17:34:38 +0100

    cms

---
+++ tema9/c91/ej7_poo/ExpocochesCampanillas.java
@@ -0,0 +1,16 @@
+/**
+  @author: Christian Millán Soria
+  @file: ExpocochesCampanillas.java
+  @info: clase "ExpocochesCampanillas" - ejercicio 7
+*/
+
+package tema9.c91.ej7_poo;
+
+public class ExpocochesCampanillas{
+  public static void main(String[] args){
+    // instancias de la clase "Zona"
+      Zona salaPrincipal=new Zona(1000);
+      Zona zonaCompraVenta=new Zona(200);
+      Zona zonaVip=new Zona(25);
+  }
+}
