6e9e935725887abe68eab24233a0015ba105d0d3
cmilsor245 <cmilsor245@g.educaand.es>
2023-03-02 18:35:36 +0100

    cms

---
+++ tema9/c91/ej9_poo/Movil.java
@@ -0,0 +1,26 @@
+/**
+  @author: Christian Millán Soria
+  @file: Movil.java
+  @info: clase "Movil" - ejercicio 9
+*/
+
+package tema9.c91.ej9_poo;
+
+public class Movil extends Terminal{
+  // atributos
+    private String tarifa;
+
+  /*********************************/
+
+  // constructor
+    public Movil(String n, String t){
+      super(n);
+
+      this.tarifa=t;
+    }
+
+  /*********************************/
+
+  // métodos
+    
+}
