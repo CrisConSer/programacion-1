05852006d9e505cf41a222b78f9529367d3e703e
cmilsor245 <cmilsor245@g.educaand.es>
2023-03-02 18:12:27 +0100

    cms

--- tema9/c91/ej8_poo/Terminal.java
+++ tema9/c91/ej8_poo/Terminal.java
@@ -7,5 +7,34 @@
 package tema9.c91.ej8_poo;
 
 public class Terminal{
-  
+  // atributos
+    private String numero;
+    private int tiempoConversacion;
+
+  /*****************************/
+
+  // constructor
+    public Terminal(String n){
+      numero=n;
+
+      tiempoConversacion=0;
+    }
+
+    /*****************************/
+
+    // métodos
+      /**
+        @info: genera una llamada de un terminal a otro
+        @param n: segundos de la conversación
+      */
+        public void llama(Terminal t, int segundos){
+          tiempoConversacion+=segundos;
+
+          t.tiempoConversacion+=segundos;
+        }
+
+      // método "toString"
+        public String toString(){
+          return "Nº "+numero+" - "+tiempoConversacion+"s de conversación.";
+        }
 }
