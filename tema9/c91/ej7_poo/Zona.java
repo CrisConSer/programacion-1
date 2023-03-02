41102a062c2e694762b67a9297b55c619969801d
cmilsor245 <cmilsor245@g.educaand.es>
2023-03-02 16:59:01 +0100

    cms

--- tema9/c91/ej7_poo/Zona.java
+++ tema9/c91/ej7_poo/Zona.java
@@ -20,5 +20,25 @@ public class Zona{
   /*****************************************/
 
   // métodos
-    
+    // método para obtener el número de entradas por vender
+      public int getEntradasPorVender(){
+        return entradasPorVender;
+      }
+
+    /******************/
+
+    // método para vender un número de entradas (comprueba si quedan entradas libres antes de realizar la venta)
+      public void vender(int n){
+        if(this.entradasPorVender==0){
+          System.out.print("Lo siento, las entradas para esa zona están agotadas.");
+        }else if(this.entradasPorVender<n){
+          System.out.print("Sólo quedan "+this.entradasPorVender+" entradas para esa zona.");
+        }
+
+        if(this.entradasPorVender>=n){
+          entradasPorVender-=n;// eliminación de las entradas que se van a vender
+
+          System.out.print("Aquí tienes tus "+n+" entradas, gracias.");
+        }
+      }
 }
