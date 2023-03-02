5832c485f5f505f3ba9dc85d3c0ee97c2c8a4b6c
cmilsor245 <cmilsor245@g.educaand.es>
2023-03-02 16:32:14 +0100

    cms

--- tema9/c91/ej7_poo/Zona.java
+++ tema9/c91/ej7_poo/Zona.java
@@ -7,5 +7,18 @@
 package tema9.c91.ej7_poo;
 
 public class Zona{
-  
+  // atributos
+    private int entradasPorVender;
+
+  /*****************************************/
+
+  // constructor
+    public Zona(int n){
+      entradasPorVender=n;
+    }
+
+  /*****************************************/
+
+  // métodos
+    
 }
