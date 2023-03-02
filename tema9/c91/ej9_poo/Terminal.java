ee2f8bf597a0c985fd19cabd1254316cf17497bf
cmilsor245 <cmilsor245@g.educaand.es>
2023-03-02 18:22:25 +0100

    cms

---
+++ tema9/c91/ej9_poo/Terminal.java
@@ -0,0 +1,40 @@
+/**
+  @author: Christian Millán Soria
+  @file: Terminal.java
+  @info: clase "Terminal" - ejercicio 8
+*/
+
+package tema9.c91.ej9_poo;
+
+public class Terminal{
+  // atributos
+    private String numero;
+    private int tiempoConversacion;
+
+  /*****************************/
+
+  // constructor
+    public Terminal(String n){
+      numero=n;
+
+      tiempoConversacion=0;
+    }
+
+    /*****************************/
+
+    // métodos
+      /**
+        @info: genera una llamada de un terminal a otro
+        @param n: segundos de la conversación
+      */
+        public void llama(Terminal t, int segundos){
+          tiempoConversacion+=segundos;
+
+          t.tiempoConversacion+=segundos;
+        }
+
+      // método "toString"
+        public String toString(){
+          return "Nº "+numero+" - "+tiempoConversacion+"s de conversación.";
+        }
+}
---
+++ tema9/c91/ej9_poo/TestEj9.java
@@ -0,0 +1,11 @@
+/**
+  @author: Christian Millán Soria
+  @file: TestEj9.java
+  @info: clase "TestEj9" - ejercicio 9
+*/
+
+package tema9.c91.ej9_poo;
+
+public class TestEj9{
+  
+}
