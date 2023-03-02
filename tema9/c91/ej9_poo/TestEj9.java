5dd68ca11b4a652bc7d6d67302f7854f30dd5b6f
cmilsor245 <cmilsor245@g.educaand.es>
2023-03-02 18:28:28 +0100

    cms

--- tema9/c91/ej9_poo/TestEj9.java
+++ tema9/c91/ej9_poo/TestEj9.java
@@ -9,9 +9,9 @@ package tema9.c91.ej9_poo;
 public class TestEj9{
   public static void main(String[] args){
     // instancias
-      Movil m1=new Movil("678 11 22 33");
-      Movil m2=new Movil("644 74 44 69");
-      Movil m3=new Movil("622 32 89 09");
+      Movil m1=new Movil("678 11 22 33", "rata");
+      Movil m2=new Movil("644 74 44 69", "mono");
+      Movil m3=new Movil("622 32 89 09", "bisonte");
 
     /***************************************/
 
