dc72f91c84e1b5ea593c1a88873246e02e46459e
cmilsor245 <cmilsor245@g.educaand.es>
2023-03-08 18:59:40 +0100

    cms

--- tema9/c92/ej5_arrays/TestEj5Arrays.java
+++ tema9/c92/ej5_arrays/TestEj5Arrays.java
@@ -6,6 +6,30 @@
 
 package tema9.c92.ej5_arrays;
 
+import java.util.Scanner;
+
 public class TestEj5Arrays{
-  
+  public static void main(String[] args){
+    // menú
+      System.out.println("1. Listado");
+      System.out.println("2. Alta");
+      System.out.println("3. Baja");
+      System.out.println("4. Modificación");
+      System.out.println("5. Entrada de mercancía");
+      System.out.println("6. Salida de mercancía");
+      System.out.println("7. Salir");
+
+      // scanner
+        System.out.print("\nElige una opción: ");
+          Scanner input=new Scanner(System.in);
+            int opcion=input.nextInt();
+
+      /**************************************/
+
+      switch(opcion){
+        case 1:
+          
+        break;
+      }
+  }
 }
