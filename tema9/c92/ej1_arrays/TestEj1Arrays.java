bb0916d9bbd95184e0ee9b6bb185ffbaeb38a3e3
cmilsor245 <cmilsor245@g.educaand.es>
2023-03-08 16:59:45 +0100

    cms

---
+++ tema9/c92/ej1_arrays/TestEj1Arrays.java
@@ -0,0 +1,11 @@
+/**
+  @author: Christian Millán Soria
+  @file: TestEj1Arrays.java
+  @info: programa principal del ejercicio 1 de arrays
+*/
+
+package tema9.c92.ej1_arrays;
+
+public class TestEj1Arrays{
+  
+}
