9a516cb8d0bbc4a2ea577f1ad304520a35947016
cmilsor245 <cmilsor245@g.educaand.es>
2023-03-08 23:43:24 +0100

    cms

--- tema9/c92/ej3_arrays/TestEj3Arrays.java
+++ tema9/c92/ej3_arrays/TestEj3Arrays.java
@@ -1,5 +1,11 @@
+/**
+  @author: Christian Millán Soria
+  @file: TestEj3Arrays.java
+  @info: programa principal del ejercicio 3 de arrays
+*/
+
 package tema9.c92.ej3_arrays;
 
-public class TestEj3Arrays {
+public class TestEj3Arrays{
   
 }
