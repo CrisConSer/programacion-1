c25e449ee8cf5aede7086827b25397f98b5a9cb3
cmilsor245 <cmilsor245@g.educaand.es>
2023-03-09 10:31:51 +0100

    cms

--- tema9/c92/ej3_arrays/TestEj3Arrays.java
+++ tema9/c92/ej3_arrays/TestEj3Arrays.java
@@ -19,9 +19,16 @@ public class TestEj3Arrays{
 
       /***************/
 
-      // instancias
+      // instancias de base
         for(int i=0; i<t; i++){
           discos[i]=new Disco();
         }
+
+      /***************/
+
+      // discos nuevos
+        discos[0]=new Disco("R4T8K9", "The Electric Sunflowers", "Midnight in the Metropolis", "Synthwave", 4);
+        discos[0]=new Disco("J6M9P2", "The Cosmic Wanderers", "Interstellar Dreams", "Space Rock", 5);
+        discos[0]=new Disco("L8B3N6", "The Neon Nights", "City of Dreams", "Retrowave", 3);
   }
 }
