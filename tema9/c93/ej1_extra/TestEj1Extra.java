c630ef68f8e1db6ac8e6a680740aff2bf71403ef
cmilsor245 <cmilsor245@g.educaand.es>
2023-03-14 19:14:51 +0100

    cms

--- tema9/c93/ej1_extra/TestEj1Extra.java
+++ tema9/c93/ej1_extra/TestEj1Extra.java
@@ -6,6 +6,17 @@
 
 package tema9.c93.ej1_extra;
 
+import tema9.c93.ej1_extra.producto.Producto;;
+
 public class TestEj1Extra{
-  
+  // array de objetos
+    // tamaño
+      int t=5;
+
+    // creación
+      Producto[] productos=new Producto[t];
+
+  /*************************************/
+
+  // 
 }
