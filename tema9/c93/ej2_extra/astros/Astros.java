96334c02d4be7663ab70585eda2f0ef9d2bbe094
cmilsor245 <cmilsor245@g.educaand.es>
2023-03-16 19:54:57 +0100

    cms

--- tema9/c93/ej2_extra/astros/Astros.java
+++ tema9/c93/ej2_extra/astros/Astros.java
@@ -7,5 +7,29 @@
 package tema9.c93.ej2_extra.astros;
 
 public abstract class Astros{
-  
+  // atributos
+    private double radioEcuatorial;
+    private double rotacionSobreSuEje;
+    private double masa;
+    private double temperaturaMedia;
+    private double gravedad;
+
+  /*******************************************/
+
+  // métodos
+    /**
+      @name: "muestra"
+      @info: muestra toda la información de la que se dispone sobre el astro
+      @return: 
+    */
+      public String muestra(){
+        String info="--------------------------";
+          info+="- Radio Ecuatorial: "+this.radioEcuatorial;
+          info+="- Rotación sobre su eje: "+this.rotacionSobreSuEje;
+          info+="- Masa: "+this.masa;
+          info+="Temperatura media: "+this.temperaturaMedia;
+          info+="Gravedad: "+this.gravedad;
+
+        return info;
+      }
 }
---
+++ tema9/c93/ej2_extra/astros/Planetas.java
@@ -0,0 +1,11 @@
+/**
+  @author: Christian Millán Soria
+  @file: Planetas.java
+  @info: clase "Planetas"
+*/
+
+package tema9.c93.ej2_extra.astros;
+
+public class Planetas{
+  
+}
