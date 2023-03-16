d0956ff5b82f1d4477af3a9d57457164d79e19b3
cmilsor245 <cmilsor245@g.educaand.es>
2023-03-17 00:07:28 +0100

    cms

--- tema9/c93/ej2_extra/TestEj2Extra.java
+++ tema9/c93/ej2_extra/TestEj2Extra.java
@@ -17,5 +17,12 @@ public class TestEj2Extra{
         Planetas[] arrayPlanetas=new Planetas[8];
 
         arrayPlanetas[0]=new Planetas(2439.7, 1408, 33011, 167, 3.7, "Mercurio", 57.91, 87.97);
+        arrayPlanetas[1]=new Planetas();
+        arrayPlanetas[2]=new Planetas();
+        arrayPlanetas[3]=new Planetas();
+        arrayPlanetas[4]=new Planetas();
+        arrayPlanetas[5]=new Planetas();
+        arrayPlanetas[6]=new Planetas();
+        arrayPlanetas[7]=new Planetas();
   }
 }
