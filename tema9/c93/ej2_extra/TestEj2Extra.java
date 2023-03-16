e1f29f070f2a829344545fdc8c6ad17990314478
cmilsor245 <cmilsor245@g.educaand.es>
2023-03-17 00:13:25 +0100

    cms

--- tema9/c93/ej2_extra/TestEj2Extra.java
+++ tema9/c93/ej2_extra/TestEj2Extra.java
@@ -16,13 +16,13 @@ public class TestEj2Extra{
       // planetas
         Planetas[] arrayPlanetas=new Planetas[8];
 
-        arrayPlanetas[0] = new Planetas(2439.7, 1408, 33011, 167, 3.7, "Mercurio", 57.91, 87.97);
-        arrayPlanetas[1] = new Planetas(6051.8, 5832.5, 48675, 462, 8.87, "Venus", 108.2, 224.7);
-        arrayPlanetas[2] = new Planetas(6371, 24, 5.972e24, 15, 9.81, "Tierra", 149.6, 365.24);
-        arrayPlanetas[3] = new Planetas(3389.5, 24.6, 6.4171e23, -63, 3.71, "Marte", 227.9, 686.98);
-        arrayPlanetas[4] = new Planetas(69911, 9.9, 1.8982e27, -108, 24.79, "Júpiter", 778.5, 4332.59);
-        arrayPlanetas[5] = new Planetas(58232, 10.7, 5.6834e26, -139, 10.44, "Saturno", 1.4e9, 10759.22);
-        arrayPlanetas[6] = new Planetas(25362, 17.2, 8.681e25, -197, 8.87, "Urano", 2.87e9, 30688.5);
-        arrayPlanetas[7] = new Planetas(24622, 16.1, 1.0243e26, -201, 11.15, "Neptuno", 4.5e9, 60182);
+        arrayPlanetas[0]=new Planetas(2439.7, 1408, 33011, 167, 3.7, "Mercurio", 57.91, 87.97);
+        arrayPlanetas[1]=new Planetas(6051.8, 5832.5, 48675, 462, 8.87, "Venus", 108.2, 224.7);
+        arrayPlanetas[2]=new Planetas(6371, 24, 5.972e24, 15, 9.81, "Tierra", 149.6, 365.24);
+        arrayPlanetas[3]=new Planetas(3389.5, 24.6, 6.4171e23, -63, 3.71, "Marte", 227.9, 686.98);
+        arrayPlanetas[4]=new Planetas(69911, 9.9, 1.8982e27, -108, 24.79, "Júpiter", 778.5, 4332.59);
+        arrayPlanetas[5]=new Planetas(58232, 10.7, 5.6834e26, -139, 10.44, "Saturno", 1.4e9, 10759.22);
+        arrayPlanetas[6]=new Planetas(25362, 17.2, 8.681e25, -197, 8.87, "Urano", 2.87e9, 30688.5);
+        arrayPlanetas[7]=new Planetas(24622, 16.1, 1.0243e26, -201, 11.15, "Neptuno", 4.5e9, 60182);
   }
 }
