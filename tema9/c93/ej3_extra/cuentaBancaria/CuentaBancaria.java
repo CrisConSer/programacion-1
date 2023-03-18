a9e50760ffbb578335da33d139e78941e9fadb9c
cmilsor245 <cmilsor245@g.educaand.es>
2023-03-18 17:25:56 +0100

    cms

---
+++ tema9/c93/ej3_extra/cuentaBancaria/CuentaBancaria.java
@@ -0,0 +1,26 @@
+/**
+  @author: Christian Millán Soria
+  @file: CuentaBancaria.java
+  @info: clase abstracta "CuentaBancaria"
+*/
+
+package tema9.c93.ej3_extra.cuentaBancaria;
+
+public abstract class CuentaBancaria{
+  // atributos
+    private String iban;
+    private double saldo;
+
+  /**********************************/
+
+  // constructor
+    public CuentaBancaria(String iban, double saldo){
+      this.iban=iban;
+      this.saldo=saldo;
+    }
+
+  /**********************************/
+
+  // métodos
+    
+}
