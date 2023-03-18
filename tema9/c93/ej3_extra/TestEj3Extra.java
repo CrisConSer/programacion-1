2b7e7158ae73dcb5a81026d7c8e14cf28077e195
cmilsor245 <cmilsor245@g.educaand.es>
2023-03-18 19:16:51 +0100

    cms

--- tema9/c93/ej3_extra/TestEj3Extra.java
+++ tema9/c93/ej3_extra/TestEj3Extra.java
@@ -8,8 +8,15 @@ package tema9.c93.ej3_extra;
 
 import tema9.c93.ej3_extra.cuentaBancaria.CuentaBancaria;
 
+import java.util.Scanner;
+
 public class TestEj3Extra{
   public static void main(String[] args){
-    
+    // menú
+      System.out.println("1. Listado");
+      System.out.println("2. Ingresar dinero");
+      System.out.println("3. Retirar dinero");
+      System.out.println("4. Traspasar dinero");
+      System.out.println("5. Calcular el interés");
   }
 }
