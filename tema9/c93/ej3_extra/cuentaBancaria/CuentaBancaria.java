5cf35d91c46ccf2a5d4d60d286eb6b1de9c31935
cmilsor245 <cmilsor245@g.educaand.es>
2023-03-18 17:34:21 +0100

    cms

--- tema9/c93/ej3_extra/cuentaBancaria/CuentaBancaria.java
+++ tema9/c93/ej3_extra/cuentaBancaria/CuentaBancaria.java
@@ -41,5 +41,18 @@ public abstract class CuentaBancaria{
       @info: aumentar el valor del atributo "saldo"
       @param double saldo: valor a sumar al saldo actual
     */
-      
+      public void ingresar(double saldo){
+        this.saldo+=saldo;
+      }
+
+    /********************/
+
+    /**
+      @name: retirar
+      @info: disminuir el valor del atributo "saldo"
+      @param double saldo: valor a restar al saldo actual
+    */
+      public void retirar(double saldo){
+        this.saldo-=saldo;
+      }
 }
