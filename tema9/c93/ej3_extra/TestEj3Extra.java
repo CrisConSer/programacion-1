1b848d94b2c41d27263ee279364af5ae2c309984
cmilsor245 <cmilsor245@g.educaand.es>
2023-03-19 11:53:18 +0100

    cms

--- tema9/c93/ej3_extra/TestEj3Extra.java
+++ tema9/c93/ej3_extra/TestEj3Extra.java
@@ -12,12 +12,24 @@ import java.util.Scanner;
 
 public class TestEj3Extra{
   public static void main(String[] args){
+    // scanner
+      Scanner input=new Scanner(System.in);
+        int opcion;
+
+    /*******************************************/
+
     // menú
-      System.out.println("1. Listado");
-      System.out.println("2. Mostrar información");
-      System.out.println("3. Ingresar dinero");
-      System.out.println("4. Retirar dinero");
-      System.out.println("5. Traspasar dinero");
-      System.out.println("6. Calcular el interés");
+      do{
+        System.out.println("1. Listado");
+        System.out.println("2. Mostrar información");
+        System.out.println("3. Ingresar dinero");
+        System.out.println("4. Retirar dinero");
+        System.out.println("5. Traspasar dinero");
+        System.out.println("6. Calcular el interés");
+        System.out.println("7. Salir");
+
+        System.out.print("Selecciona una opción: ");
+          opcion=input.nextInt();
+      }while(opcion!=7);
   }
 }
