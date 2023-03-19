21b3327ec69dc541412e53b165b1e840df21a6b5
cmilsor245 <cmilsor245@g.educaand.es>
2023-03-19 11:05:24 +0100

    cms

---
+++ tema9/c93/ej3_extra/cuentaBancaria/CuentaAhorro.java
@@ -0,0 +1,11 @@
+/**
+  @author: Christian Millán Soria
+  @file: CuentaAhorro.java
+  @info: subclase de "CuentaBancaria"
+*/
+
+package tema9.c93.ej3_extra.cuentaBancaria;
+
+public class CuentaAhorro{
+  
+}
---
+++ tema9/c93/ej3_extra/cuentaBancaria/CuentaCorriente.java
@@ -0,0 +1,11 @@
+/**
+  @author: Christian Millán Soria
+  @file: CuentaCorriente.java
+  @info: subclase de "CuentaBancaria"
+*/
+
+package tema9.c93.ej3_extra.cuentaBancaria;
+
+public class CuentaCorriente{
+  
+}
